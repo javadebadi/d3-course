d543f27c0a3bf8ff4a333e3433a3d8885c36a325
javadebadi <javad.ebadi.1990@gmail.com>
2022-05-31 00:34:54 +0430

    Added an example of simple bar chart

---
+++ 0050/script.js
@@ -0,0 +1,21 @@
+const dataset = [10, 20, 30, 50, 60, 40, 90]
+// const dataset = [10, 20, 50]
+const w = 600
+const h = 300
+
+// create svg element with determined width and hight
+const svg = d3.select("body")
+              .append("svg")
+              .attr("width", w)
+              .attr("height", h)
+
+// add bars
+svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("x", (d,  i) => i * 75)
+   .attr("y", (d, i) => (h-d*2))
+   .attr("fill", "blue")
+   .attr("width", 60)
+   .attr("height", d => d*2)
